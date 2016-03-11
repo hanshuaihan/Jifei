82a8fb726eef1d1a59ecde9ed6e2263d496032e5
unknown <670654360@qq.com>
2016-03-12 03:46:18 +0800

    first call db create

--- src/com/jifei/app/JifeiOpenHelper.java
+++ src/com/jifei/app/JifeiOpenHelper.java
@@ -5,33 +5,35 @@ import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 public class JifeiOpenHelper extends SQLiteOpenHelper{
 	
 	//traffic表创建
-	public static final String CREATE_Traffic="create table Traffic("
+	public static final String CREATE_TRAFFIC="create table Traffic("
 			+"id integer primary key autoincrement,"
-			+"traffic_number text,";
+			+"traffic_number text)";
 	//call表创建
-	public static final String CREATE_Call="create table Call("
+	public static final String CREATE_CALL="create table Call("
 			+"id integer primary key autoincrement,"
 			+"call_in_time text,"
 			+"call_out_time text,"
 			+"call_money_spend text,"
-			+"call_money text,)";
+			+"call_money text)";
 	//SMS表创建
-	public static final String CREATE_Sms="create table SMS("
+	public static final String CREATE_SMS="create table Sms("
 			+"id integer primary key autoincrement,"
-			+"Sms_sendnumber text"
-			+"Sms_money text)";
+			+"sms_sendnumber text"
+			+"sms_money text)";
 	public JifeiOpenHelper(Context context,String name,CursorFactory factory,int version){
 		super(context,name,factory,version);
 	}
 	@Override
 	public void onCreate(SQLiteDatabase db){
-		db.execSQL(CREATE_Traffic);
-		db.execSQL(CREATE_Call);
-		db.execSQL(CREATE_Sms);
+		db.execSQL(CREATE_TRAFFIC);
+		db.execSQL(CREATE_CALL);
+		db.execSQL(CREATE_SMS);
+
 	}
 	@Override
 	public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion)
--- src/com/jifei/app/MainActivity.java
+++ src/com/jifei/app/MainActivity.java
@@ -5,6 +5,7 @@ import com.jifei.app.R;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -16,14 +17,18 @@ import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 
 import android.content.BroadcastReceiver;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 public class MainActivity extends Activity {
+	//定义用到的控件
 	private TextView liuliangshengyu;
 	private Button xiaozhengliuliang;
 	private TextView hurushichang;
@@ -37,12 +42,14 @@ public class MainActivity extends Activity {
 	private MessageReceiver messageReceiver;
 	private IntentFilter receiveFilter;
 	private static final String UNICOM = "10010";
+	private JifeiOpenHelper dbHelper;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		dbHelper=new JifeiOpenHelper(this,"Jifei.db",null,1);//创建JifeiOpenHelper对象，命名为Jifei。db
 		xiaozhengliuliang=(Button)findViewById(R.id.xiaozhengliuliang);
 		huafeichaxun=(Button)findViewById(R.id.huafeichaxun);
 		hurushichang=(TextView)findViewById(R.id.hurushichang);
@@ -54,11 +61,22 @@ public class MainActivity extends Activity {
 		mWaterWaveView = (WaterWaveView) findViewById(R.id.wave_view);
         mWaterWaveView.setmWaterLevel(0.8F);
 		mWaterWaveView.startWave();
+		//所有用到的UI控件的创建
 		receiveFilter=new IntentFilter();
         receiveFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
         messageReceiver=new MessageReceiver();
         registerReceiver(messageReceiver,receiveFilter);
-		//TEXTVIEW和BUTTON的注册
+        //在创建活动时调用数据库搜索第一行并显示
+        SQLiteDatabase db=dbHelper.getWritableDatabase();
+        Cursor cursor=db.query("Call", null, null, null, null, null,null);
+        if(cursor.moveToFirst()){
+        	do{
+        		String call_money=cursor.getString(cursor.getColumnIndex("call_money"));
+        		shengyuhuafei.setText(call_money+"元");
+        	}while(cursor.moveToNext());
+        	cursor.close();
+        }
+        //所有控件注册
 		huafeichaxun.setOnClickListener(new OnClickListener(){
         	@Override
         	public void onClick(View v){
@@ -66,6 +84,8 @@ public class MainActivity extends Activity {
         		Intent sentIntent=new Intent("SENT_SMS_ACTION");
         		PendingIntent pi=PendingIntent.getBroadcast(MainActivity.this, 0, sentIntent, 0);
         		smsManager.sendTextMessage(UNICOM,null,"102",null,null);
+        		dbHelper.getWritableDatabase();
+        		Toast.makeText(getApplication(), "请稍等。。。", Toast.LENGTH_LONG).show();
         	}
     });
 	
@@ -79,27 +99,34 @@ public class MainActivity extends Activity {
     	}
 });
 }
-    
-class MessageReceiver extends BroadcastReceiver{
-	
-	@Override
-	public void onReceive(Context context,Intent intent){
-		Bundle bundle=intent.getExtras();
-		Object[]pdus=(Object[])bundle.get("pdus");
-		SmsMessage[]messages=new SmsMessage[pdus.length];
-		for(int i=0;i<messages.length;i++){
-			messages[i]=SmsMessage.createFromPdu((byte[])pdus[i]);
-		}
-		String ss="";
-		for (SmsMessage message : messages){
-			ss +=message.getMessageBody();
-		}
-		String str1=ss.substring(0,ss.indexOf("元"));
-		String str2=ss.substring(0,ss.indexOf("余额为"));
-		ss=str1.replace(str2,"");
+	//广播器拦截短信截取字符并显示
+	class MessageReceiver extends BroadcastReceiver{
 		
-		shengyuhuafei.setText(ss+"元");
-	}
+		@Override
+		public void onReceive(Context context,Intent intent){
+			Bundle bundle=intent.getExtras();
+			Object[]pdus=(Object[])bundle.get("pdus");
+			SmsMessage[]messages=new SmsMessage[pdus.length];
+			for(int i=0;i<messages.length;i++){
+				messages[i]=SmsMessage.createFromPdu((byte[])pdus[i]);
+			}
+			String ss="";
+			for (SmsMessage message : messages){
+				ss +=message.getMessageBody();
+			}
+			String str1=ss.substring(0,ss.indexOf("元"));
+			String str2=ss.substring(0,ss.indexOf("余额为"));
+			ss=str1.replace(str2,"");
+			
+			shengyuhuafei.setText(ss+"元");
+			SQLiteDatabase db=dbHelper.getWritableDatabase();
+			ContentValues values = new ContentValues();
+			values.put("call_money", ss);
+			db.insert("Call", null, values);
+		}
+
 }
+	
 }
+
 
